69e2696ef64d26b616ccc4691f0b3ce709fcdb9b
Nikita <nikita.gubarev31@gmail.com>
2023-09-16 09:48:42 +0300

    Add gRPC server addresses into the config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 from dotenv import load_dotenv
 
@@ -9,3 +9,7 @@ SQLALCHEMY_DATABASE_URL = str(os.getenv("DATABASE_URL"))
 
 JWT_SECRET_KEY = str(os.getenv("JWT_SECRET_KEY"))
 JWT_REFRESH_SECRET_KEY = str(os.getenv("JWT_REFRESH_SECRET_KEY"))
+
+BLOG_GRPC_SERVER_ADDR = "0.0.0.0:50051"
+AUTH_GRPC_SERVER_ADDR = "0.0.0.0:50052"
+USER_GRPC_SERVER_ADDR = "0.0.0.0:50053"
