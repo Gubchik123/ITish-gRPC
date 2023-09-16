b35d6a2ffe61f885197494af6df4751bb46ecd3a
Nikita <nikita.gubarev31@gmail.com>
2023-09-16 11:24:24 +0300

    Move the grpc_services/blog.py into new blog/grpc_service.py

--- grpc_servers/blog.py
+++ grpc_servers/blog.py
@@ -1,11 +1,11 @@
 import asyncio
 
 from db import Base, engine
-from grpc_services import blog
+from blog import grpc_service
 from config import BLOG_GRPC_SERVER_ADDR
 
 
 if __name__ == "__main__":
     print("Start Blog gRPC server")
     Base.metadata.create_all(bind=engine)
-    asyncio.run(blog.start(BLOG_GRPC_SERVER_ADDR))
+    asyncio.run(grpc_service.start(BLOG_GRPC_SERVER_ADDR))
