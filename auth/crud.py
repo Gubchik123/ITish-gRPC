33b33ceaf9137eacf4157826641691732a61f355
Nikita <nikita.gubarev31@gmail.com>
2023-09-18 18:02:37 +0300

    Remove checking if user is None from the auth/crud.py

--- auth/crud.py
+++ auth/crud.py
@@ -1,5 +1,3 @@
-from sqlalchemy.orm.exc import NoResultFound
-
 from models import User
 from db import SessionLocal, add_commit_and_refresh
 
@@ -9,7 +7,7 @@ from .protos.auth_pb2 import UserSchema
 def get_user_with_(user_schema: UserSchema):
     """Returns user by email and username in the given UserSchema."""
     with SessionLocal() as db:
-        user = (
+        return (
             db.query(User)
             .filter(
                 User.email == user_schema.email,
@@ -17,28 +15,22 @@ def get_user_with_(user_schema: UserSchema):
             )
             .first()
         )
-    if user is None:
-        raise NoResultFound
-    return user
 
 
 def get_user_by_(email: str):
     """Returns user by the given email."""
     with SessionLocal() as db:
-        user = db.query(User).filter(User.email == email).first()
-    if user is None:
-        raise NoResultFound
-    return user
+        return db.query(User).filter(User.email == email).first()
 
 
 def create_user(user_schema: UserSchema):
     """Creates and returns user by data in the given UserSchema."""
     with SessionLocal() as db:
         return add_commit_and_refresh(
-            db, 
+            db,
             User(
                 username=user_schema.username,
                 email=user_schema.email,
                 password=user_schema.password,
-            )
+            ),
         )
