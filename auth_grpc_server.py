baf695e372ce73648530dc94342e4bd84ab7776b
Nikita <nikita.gubarev31@gmail.com>
2023-09-18 15:25:16 +0300

    Move the grpc_servers/auth.py into new auth_grpc_server.py

--- auth_grpc_server.py
+++ auth_grpc_server.py
@@ -6,6 +6,5 @@ from config import AUTH_GRPC_SERVER_ADDR
 
 
 if __name__ == "__main__":
-    print("Start auth gRPC server")
     Base.metadata.create_all(bind=engine)
     asyncio.run(grpc_service.start(AUTH_GRPC_SERVER_ADDR))
