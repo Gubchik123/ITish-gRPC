c815562aa553c33c6924be3323c048cef0edf186
Nikita <nikita.gubarev31@gmail.com>
2023-09-19 10:40:02 +0300

    Move the grpc_servers/user.py into new user_grpc_server.py

--- user_grpc_server.py
+++ user_grpc_server.py
@@ -6,6 +6,5 @@ from config import USER_GRPC_SERVER_ADDR
 
 
 if __name__ == "__main__":
-    print("Start user gRPC server")
     Base.metadata.create_all(bind=engine)
     asyncio.run(grpc_service.start(USER_GRPC_SERVER_ADDR))
